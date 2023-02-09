33c41b788e18a8f52adde795e7ade3f7d0d7880d
nanomarquez <fmarquez@aimonkey.io>
2023-02-09 20:22:52 -0300

    p

--- client/src/components/CardsOne.jsx
+++ client/src/components/CardsOne.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function CardsOne({img,title,parrafe}) {
   return (
-    <div className="bg-white shadow-md w-[350px] p-5 h-[350px] rounded-md flex flex-col items-center justify-evenly text-center">
+    <div className="bg-white text-black shadow-md w-[350px] p-5 h-[350px] rounded-md flex flex-col items-center justify-evenly text-center">
       <img src={img} alt={title} className='w-24'/>
       <h2 className='font-bold'>{title}</h2>
-      <p>{parrafe}</p>
+      <p className='text-black'>{parrafe}</p>
     </div>
   )
 }
