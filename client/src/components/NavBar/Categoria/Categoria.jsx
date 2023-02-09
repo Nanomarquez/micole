f060934b33c5859657310527fb35337a83d22769
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-02-09 20:10:32 -0300

    slide categoria

---
+++ client/src/components/NavBar/Categoria/Categoria.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import style from "./Contacto.module.css"
+export default function Categoria() {
+  return (
+    <div className={style.layout}>Categoria</div>
+  )
+}
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Burguer from "./svg/Burguer";
-import Logo from "../../assets/logo1.png"
+import Logo from "../../assets/logo1.png";
 import style from "./NavBar.module.css";
+import Categoria from "./Categoria/Categoria";
 import { Link } from "react-router-dom";
 function NavBar() {
-  
+  const [OpenCategory, setOpenCategory] = useState(false);
+  const ToggleCategory = () => {
+    setOpenCategory(!OpenCategory);
+  };
   return (
     <div className={style.layout}>
-       <img  className={style.img} src={Logo}/>
+      <img className={style.img} src={Logo} />
       <div className={style.container}>
-       
         <div className={style.items}>
           <p className={style.p}>Inicio</p>
-          <p className={style.p}>Categorias</p>
+          <p className={style.p} onClick={ToggleCategory}>
+            Categorias
+          </p>
           <p className={style.p}>BLOG</p>
           <p className={style.p}>Contáctanos</p>
         </div>
@@ -21,14 +26,14 @@ function NavBar() {
           <Burguer />
         </div>
         <div className={style.buttonContainer}>
-          
           <button className={style.SesionButtom}>Iniciar sesion</button>
-        
-          <Link  to={"/enroll"}>
-          <button className={style.SesionButtom}>Inscribe tu colegio</button>
-          </Link >
+
+          <Link to={"/enroll"}>
+            <button className={style.SesionButtom}>Inscribe tu colegio</button>
+          </Link>
         </div>
       </div>
+      {OpenCategory && <Categoria />}
     </div>
   );
 }
