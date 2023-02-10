c3f632b79ac121c1753b0b937189fc0858c2a55a
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-02-09 22:57:58 -0300

    content home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Footer from './components/Footer'
 import NavBar from './components/NavBar/NavBar'
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import EnrollSchool from './pages/EnrollSchool';
 function App() {
   return (
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import style from "./Home.module.css";
+import fondoHome from "../../assets/Home_img.png";
+import Icon_directo from "./svg/Icon_directo";
+import Icon_recomendacion from "./svg/Icon_recomendacion";
+import Icon_school from "./svg/Icon_school";
+function Home() {
+  return (
+    <>
+     <div className={style.container}>
+      <img className={style.img} src={fondoHome} alt="home" />
+      <div className={style.landingText}>
+        <h1 className={style.h1}> Tu búsqueda de colegios comienza aquí</h1>
+        <p className={style.p}>¡Qué Emoción!</p>
+      </div>
+
+    </div>
+    <div className={style.contentHome}>
+    
+      <h1 className={style.title}>¿Por qué escoger MiCole.pe?</h1>
+      
+       <div className={style.LayoutContent}>
+
+      
+      <div className={style.CardContainer}>
+     < Icon_school/>
+      </div>
+      <div className={style.CardContainer}>
+     < Icon_directo />
+      </div>
+      <div className={style.CardContainer}>
+     < Icon_recomendacion/>
+      </div>
+    </div>
+     </div>
+   
+    </>
+   
+  );
+}
+
+export default Home;
