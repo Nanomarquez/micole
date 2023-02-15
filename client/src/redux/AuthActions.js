227a86f97d1dba702434e3b72a82aca108e144cf
nanomarquez <fmarquez@aimonkey.io>
2023-02-15 16:06:25 -0300

    auth

--- client/src/redux/AuthActions.js
+++ client/src/redux/AuthActions.js
@@ -1,10 +1,11 @@
 import {getUser,registerUser,loginUser,logoutUser,getError,isLoading} from './AuthSlice'
 import axios from 'axios'
 
-export const getUser = (email) => (dispatch) => {
+export const getUser = () => (dispatch) => {
   dispatch(isLoading())
-  axios.get(`/auth/${email}`)
-  .then(res=>dispatch(getUser(res.data)))
+  const id = localStorage.getItem('id')
+  axios.get(`/auth/${id}`)
+  .then(res=>dispatch(getUser(res.data.user)))
   .catch(err=>dispatch(getError(err.message)))
 }
 
@@ -19,8 +20,9 @@ export const loginUser = (user) => (dispatch) => {
   dispatch(isLoading())
   axios.post('/auth/signin',{user})
   .then(res=>{
-    dispatch(loginUser(res.data))
+    dispatch(loginUser(res.data.user))
     localStorage.setItem('token', res.data.token);
+    localStorage.setItem('id', res.data.user.id);
   })
   .catch(err=>dispatch(getError(err.message)))
 }
@@ -30,6 +32,7 @@ export const logoutUser = () => (dispatch) => {
   try {
     dispatch(logoutUser())
     localStorage.removeItem('token');
+    localStorage.removeItem('id');
   } catch (err) {
     dispatch(getError(err.message))
   }
