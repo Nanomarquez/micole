3784ea6a8289d2d6f0fc748f9d9206d870de0c7b
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-16 22:09:35 +0100

    correciones css

--- client/src/components/SchoolDetail-CITAS/SecCitas.jsx
+++ client/src/components/SchoolDetail-CITAS/SecCitas.jsx
@@ -18,7 +18,7 @@ import generarCalendario from "./GenCalendario"
 // ---( SE PASAN DE STRING A PROPIEDADES )
 // --- EL FORMATO ADECUADO ES    'Jan': 'Ene'
 
-export default function SecCitas({ data,setStateBtn,stateBtn }) {
+export default function SecCitas({ data, setStateBtn, stateBtn }) {
   //data es un obj que contiene las prefencias del colegio (las que configura en su dashboard)
   console.log(data);
 
@@ -31,196 +31,194 @@ export default function SecCitas({ data,setStateBtn,stateBtn }) {
 
 
 
-  const CardsDia = ({diasSemana,fechadelDia,mesdelDia}) => {
-    console.log(diasSemana,fechadelDia,mesdelDia)
+  const CardsDia = ({ diasSemana, fechadelDia, mesdelDia }) => {
+    console.log(diasSemana, fechadelDia, mesdelDia)
     const [cardSelected, setCardSelected] = useState(false);
-    // const diasSemenaDispo = mockData.map((ele) => {
-    //   return ele.dia
-    // })
-    // console.log(diasSemenaDispo)
 
-  const handlerSelected =()=>{
-  setCardSelected(!cardSelected)
-}
+
+    const handlerSelected = () => {
+      setCardSelected(!cardSelected)
+    }
     // aca se podria hacer de data 
     return (
       <>
-      {/* {
+        {/* {
         mockData.map((d)=>{
 
         })
       } */}
-      <div  className={cardSelected && style.divBorderSelected }
-      onClick={handlerSelected}
-      >
-           <p
-          className={cardSelected ? style.p_Selected : style.p }
+        <div className={cardSelected && style.divBorderSelected}
+          onClick={handlerSelected}
         >
-          {diasSemana}
-        </p>
-        <p
-        className={cardSelected ? style.p_Selected : style.p }
-        >
-          {fechadelDia}
-        </p>
-        <p
-        className={cardSelected ? style.p_Selected : style.p }
-        >
-          {mesdelDia}
-        </p>
-      </div>
-     
+          <p
+            className={cardSelected ? style.p_Selected : style.p}
+          >
+            {diasSemana}
+          </p>
+          <p
+            className={cardSelected ? style.p_SelectedNumber : style.pNumber}
+          >
+            {fechadelDia}
+          </p>
+          <p
+            className={cardSelected ? style.p_Selected : style.p}
+          >
+            {mesdelDia}
+          </p>
+        </div>
+
       </>
     );
   };
-  const CardsDiaDesactivados = ({diasSemana,fechadelDia,mesdelDia}) => {
-    console.log(diasSemana,fechadelDia,mesdelDia)
+  const CardsDiaDesactivados = ({ diasSemana, fechadelDia, mesdelDia }) => {
+    console.log(diasSemana, fechadelDia, mesdelDia)
     const [cardSelected, setCardSelected] = useState(false);
 
 
-  const handlerSelected =()=>{
-  setCardSelected(!cardSelected)
-}
+    const handlerSelected = () => {
+      setCardSelected(!cardSelected)
+    }
 
     return (
       <>
-   
-      <div  
-      className={cardSelected && style.divBorderSelected }
-      onClick={handlerSelected}
-      >
-           <p
-          className={style.p_desactiv }
+
+        <div
+          className={cardSelected && style.divBorderSelected}
+          onClick={handlerSelected}
         >
-          {diasSemana}
-        </p>
-        <p
-        className={style.p_desactiv }
-        >
-          {fechadelDia}
-        </p>
-        <p
-        className={style.p_desactiv }
-        >
-          {mesdelDia}
-        </p>
-      </div>
-     
+          <p
+            className={style.p_desactiv}
+          >
+            {diasSemana}
+          </p>
+          <p
+            className={style.p_desactiv}
+          >
+            {fechadelDia}
+          </p>
+          <p
+            className={style.p_desactiv}
+          >
+            {mesdelDia}
+          </p>
+        </div>
+
       </>
     );
   };
-  const arrLimpio = arrCarruselOrdenado.filter((ele)=> ele.dia !="")
+  const arrLimpio = arrCarruselOrdenado.filter((ele) => ele.dia != "")
   return (
     <div className={style.slider_container}>
-  
-               
+
+
 
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={0}
         slidesPerView={5}
-    
+
         grabCursor={true}
         rewind={true}
-    
-         pagination={{ clickable: true }}
-     
+
+        pagination={{ clickable: true }}
+
 
         className={style.swiper}
       >
 
         {arrLimpio?.map((d) => {
-         console.log(d)
+          console.log(d)
           return (
             <>
               <SwiperSlide className={style.swiper_slide}>
-                
-               { d.dia != "" &&
-                 <div className={style.cardDia}>
-                  {d.diaSemana === "Sáb" && (
-                     <>
-                     <p
-                       style={{
-                         fontSize: "1.9vh",
-                         fontWeight: "400",
-                         color: "#9E9999",
-                         padding:'2px'
-                       }}
-                     >
-                           {d.diaSemana}
-                     </p>
-                     <p
-                       style={{
-                         fontSize: "2.5vh",
-                         fontWeight: "500",
-                         color: "#9E9999",
-                       }}
-                     >
-                       {d.dia}
-                     </p>
-                     <p
-                       style={{
-                         fontSize: "1.9vh",
-                         fontWeight: "400",
-                         color: "#9E9999",
-                       }}
-                     >
-                       {d.mes}
-                     </p>
-                   </>
-                  )}
-                  {d.diaSemana === "Dom" && (
-                    <>
-                      <p
-                        style={{
-                          fontSize: "1.9vh",
-                          fontWeight: "400",
-                          color: "#9E9999",
-                          padding:'2px'
-                        }}
-                      >
-                            {d.diaSemana}
-                      </p>
-                      <p
-                        style={{
-                          fontSize: "2.5vh",
-                          fontWeight: "500",
-                          color: "#9E9999",
-                        }}
-                      >
-                        {d.dia}
-                      </p>
-                      <p
-                        style={{
-                          fontSize: "1.9vh",
-                          fontWeight: "400",
-                          color: "#9E9999",
-                        }}
-                      >
-                        {d.mes}
-                      </p>
-                    </>
-                  )}
 
-                  {d.diaSemana != "Sáb"&& d.diaSemana  != "Dom"&& (
-                    <>
-                      <CardsDia diasSemana={d.diaSemana} fechadelDia={d.dia} mesdelDia={d.mes} />
-                    </>
-                  
-                  )}
-        
-                </div>
-               }
-              
+                {d.dia != "" &&
+                  <div className={style.cardDia}>
+                    {d.diaSemana === "Sáb" && (
+                      <>
+                        <p
+                          style={{
+                            fontSize: "1.9vh",
+                            fontWeight: "400",
+                            color: "#9E9999",
+                            padding: '2px'
+                          }}
+                        >
+                          {d.diaSemana}
+                        </p>
+                        <p
+                          style={{
+                            fontSize: "2.5vh",
+                            fontWeight: "500",
+                            color: "#9E9999",
+                          }}
+                        >
+                          {d.dia}
+                        </p>
+                        <p
+                          style={{
+                            fontSize: "1.9vh",
+                            fontWeight: "400",
+                            color: "#9E9999",
+                          }}
+                        >
+                          {d.mes}
+                        </p>
+                      </>
+                    )}
+                    {d.diaSemana === "Dom" && (
+                      <>
+                        <p
+                          style={{
+                            fontSize: "1.9vh",
+                            fontWeight: "400",
+                            color: "#9E9999",
+                            padding: '2px'
+                          }}
+                        >
+                          {d.diaSemana}
+                        </p>
+                        <p
+                          style={{
+                            fontSize: "2.5vh",
+                            fontWeight: "500",
+                            color: "#9E9999",
+                          }}
+                        >
+                          {d.dia}
+                        </p>
+                        <p
+                          style={{
+                            fontSize: "1.9vh",
+                            fontWeight: "400",
+                            color: "#9E9999",
+                          }}
+                        >
+                          {d.mes}
+                        </p>
+                      </>
+                    )}
+
+                    {d.diaSemana != "Sáb" && d.diaSemana != "Dom" && (
+                      <>
+                        <CardsDia diasSemana={d.diaSemana} fechadelDia={d.dia} mesdelDia={d.mes} />
+                      </>
+
+                    )}
+
+                  </div>
+                }
+
               </SwiperSlide>
             </>
-          );})
+          );
+        })
         }
       </Swiper>
 
-      <div className={style.games_slider_buttons_container }>
+      <div className={style.games_slider_buttons_container}>
 
-   {/* <button
+        {/* <button
        onClick={nextButton}
       // className={
       //  toggleButton
@@ -231,7 +229,7 @@ export default function SecCitas({ data,setStateBtn,stateBtn }) {
       right
      </button> */}
       </div>
-    
+
     </div>
   );
 }
