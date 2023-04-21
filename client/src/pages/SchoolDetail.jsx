dd2ba938f1d69a5d6c2f862e3baec1f0bc2d6f61
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-21 07:29:40 +0100

    form lista de espera modularizado

--- client/src/pages/SchoolDetail.jsx
+++ client/src/pages/SchoolDetail.jsx
@@ -50,6 +50,7 @@ import Maps from "../components/Maps";
 import Ubicacion from "./SchoolDetail/Ubicacion/Ubicacion";
 import { Tabs } from "@mui/material";
 import SwDetail from "./SchoolDetail/SwipperDetail/SwDetail";
+import FormListaEspera from "./SchoolDetail/Form-lista-espera/FormListEspera";
 
 function QuiltedImageList({ firstImage, gallery, setImage, setImages }) {
   return (
@@ -498,106 +499,108 @@ if(data.select === true){
                 <TabPanel value="5">
                   {listaParams === "true" ? (
                     // lista espera
-                    <div className="p-5 bg-white flex flex-col gap-5 rounded-md shadow-md w-full">
-                      <h2 className="font-semibold text-xl">Lista de espera</h2>
-                      <form
-                        onSubmit={handleSubmitLista}
-                        className="w-full flex flex-col gap-7"
-                      >
-                        <div className="flex w-full gap-5 justify-between">
-                          {isAuth ? (
-                            <input
-                              name="nombreLista"
-                              type="text"
-                              value={user?.nombre_responsable}
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Nombre"
-                              required
-                            />
-                          ) : (
-                            <input
-                              name="nombreLista"
-                              type="text"
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Nombre"
-                              required
-                            />
-                          )}
-                          {isAuth ? (
-                            <input
-                              name="apellidoLista"
-                              type="text"
-                              value={user?.apellidos_responsable}
-                              required
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                            />
-                          ) : (
-                            <input
-                              name="apellidoLista"
-                              type="text"
-                              required
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Apellidos"
-                            />
-                          )}
-                        </div>
-                        <div className="flex w-full gap-5 justify-between">
-                          {isAuth ? (
-                            <input
-                              name="emailLista"
-                              type="email"
-                              value={user?.email}
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Correo"
-                              required
-                            />
-                          ) : (
-                            <input
-                              name="emailLista"
-                              type="email"
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Correo"
-                              required
-                            />
-                          )}
-                          {isAuth ? (
-                            <input
-                              name="celLista"
-                              type="number"
-                              pattern="[0-9]{8,15}"
-                              value={user?.telefono}
-                              required
-                              title="Solo se permiten numeros y entre 8 y 10 caracteres"
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Celular"
-                            />
-                          ) : (
-                            <input
-                              name="celLista"
-                              type="number"
-                              pattern="[0-9]{8,15}"
-                              required
-                              title="Solo se permiten numeros y entre 8 y 10 caracteres"
-                              className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                              placeholder="Celular"
-                            />
-                          )}
-                        </div>
+                    // <div className="p-5 bg-white flex flex-col gap-5 rounded-md shadow-md w-full">
+                    //   <h2 className="font-semibold text-xl">Lista de espera</h2>
+                    //   <form
+                    //     onSubmit={handleSubmitLista}
+                    //     className="w-full flex flex-col gap-7"
+                    //   >
+                    //     <div className="flex w-full gap-5 justify-between">
+                    //       {isAuth ? (
+                    //         <input
+                    //           name="nombreLista"
+                    //           type="text"
+                    //           value={user?.nombre_responsable}
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Nombre"
+                    //           required
+                    //         />
+                    //       ) : (
+                    //         <input
+                    //           name="nombreLista"
+                    //           type="text"
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Nombre"
+                    //           required
+                    //         />
+                    //       )}
+                    //       {isAuth ? (
+                    //         <input
+                    //           name="apellidoLista"
+                    //           type="text"
+                    //           value={user?.apellidos_responsable}
+                    //           required
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //         />
+                    //       ) : (
+                    //         <input
+                    //           name="apellidoLista"
+                    //           type="text"
+                    //           required
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Apellidos"
+                    //         />
+                    //       )}
+                    //     </div>
+                    //     <div className="flex w-full gap-5 justify-between">
+                    //       {isAuth ? (
+                    //         <input
+                    //           name="emailLista"
+                    //           type="email"
+                    //           value={user?.email}
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Correo"
+                    //           required
+                    //         />
+                    //       ) : (
+                    //         <input
+                    //           name="emailLista"
+                    //           type="email"
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Correo"
+                    //           required
+                    //         />
+                    //       )}
+                    //       {isAuth ? (
+                    //         <input
+                    //           name="celLista"
+                    //           type="number"
+                    //           pattern="[0-9]{8,15}"
+                    //           value={user?.telefono}
+                    //           required
+                    //           title="Solo se permiten numeros y entre 8 y 10 caracteres"
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Celular"
+                    //         />
+                    //       ) : (
+                    //         <input
+                    //           name="celLista"
+                    //           type="number"
+                    //           pattern="[0-9]{8,15}"
+                    //           required
+                    //           title="Solo se permiten numeros y entre 8 y 10 caracteres"
+                    //           className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
+                    //           placeholder="Celular"
+                    //         />
+                    //       )}
+                    //     </div>
 
-                        <button
-                          type="submit"
-                          className="border mt-5 mx-auto px-10 py-2 rounded-md shadow-lg bg-[#0061dd] text-white duration-300 cursor-pointer"
-                        >
-                          INSCRIBIRME
-                        </button>
-                      </form>
-                      <p className="text-sm p-10">
-                        Al enviar estás aceptando los{" "}
-                        <Link className="text-[#0061dd] hover:underline">
-                          Términos y Condiciones de Uso y la Política de Privacidad
-                        </Link>
-                      </p>
-                    </div>
+                    //     <button
+                    //       type="submit"
+                    //       className="border mt-5 mx-auto px-10 py-2 rounded-md shadow-lg bg-[#0061dd] text-white duration-300 cursor-pointer"
+                    //     >
+                    //       INSCRIBIRME
+                    //     </button>
+                    //   </form>
+                    //   <p className="text-sm p-10">
+                    //     Al enviar estás aceptando los{" "}
+                    //     <Link className="text-[#0061dd] hover:underline">
+                    //       Términos y Condiciones de Uso y la Política de Privacidad
+                    //     </Link>       
+     
+                    //   </p>
+                    // </div>
+                    <FormListaEspera  gradoId={ Number(gradoParams)} año={Number(ingresoParams)}  />
                   ) : (
                     // SACAR UNA CITA
                     <div className="p-5 bg-white flex flex-col gap-5 rounded-md shadow-md w-full">
---
+++ client/src/pages/SchoolDetail/Form-citas/FormCitas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FormCitas() {
+  return (
+    <div>FormCitas</div>
+  )
+}
