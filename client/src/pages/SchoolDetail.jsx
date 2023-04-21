a5ac2171e473c547f36d76f7aee3f26a1d783e5e
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-21 08:28:50 +0100

    Modularizacion citas

--- client/src/pages/SchoolDetail.jsx
+++ client/src/pages/SchoolDetail.jsx
@@ -53,6 +53,7 @@ import SwDetail from "./SchoolDetail/SwipperDetail/SwDetail";
 import FormListaEspera from "./SchoolDetail/Form-lista-espera/FormListEspera";
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import CitasD from "./SchoolDetail/Citas/CitasD";
 function QuiltedImageList({ firstImage, gallery, setImage, setImages }) {
   return (
     <div className="w-full px-4">
@@ -150,148 +151,22 @@ function SchoolDetail() {
   }, []);
 
 
-  const [date, setDate] = React.useState(dayjs(new Date()));
-  const [time, setTime] = React.useState(dayjs("2014-08-18T08:00:00"));
 
-  const [modo, setModo] = React.useState(true);
   const [openLogin, setOpenLogin] = useState(false);
 
 
 
-  function handleChangeDateHS(data) {
-    console.log(data)
-    if (data.select === true) {
-      setCita({
-        ...cita,
-        date: data.date,
-        time: data.time
-      });
-    }
-
-  }
+ 
 
 
 
-  const [cita, setCita] = React.useState({
-    date: "",
-    time: "",
-    modo: modo ? "Presencial" : "Virtual",
-    nombre: isAuth ? user.nombre_responsable + " " + user?.apellidos_responsable : "",
-
-    celular: isAuth ? user.telefono : "",
-    correo: isAuth ? user.email : "",
-    añoIngreso: ingresoParams,
-    grado: nombre_grado
-  });
-  console.log(cita);
-  useEffect(() => {
-    setCita({
-      ...cita,
-      grado: nombre_grado
-    })
-  }, [nombre_grado])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
 
-
-    if (
-      cita.time === "" ||
-      cita.date === "" ||
-      e.target["nombre"].value === "" ||
-      e.target["cel"].value === "" ||
-      e.target["email"].value === ""
-    ) {
-      Swal.fire({
-        icon: "error",
-        title: "Algo salio mal",
-        text: "Debes llenar todos los datos para continuar",
-      });
-      return;
-    }
-    if (isAuth) {
-
-      console.log(cita);
-
-      dispatch(postCita(cita));
-    } else {
-
-      Swal.fire({
-        icon: "info",
-        title: "Inicia Sesion",
-        text: "Debes iniciar sesion o registrarte",
-
-        confirmButtonText: "Iniciar Sesion",
-
-      }).then(res => {
-        if (res.isConfirmed) {
-          setOpenLogin(true);
-        }
-      });
-      // setOpenLogin(true);
-      return;
-
-    }
-  };
-
-  const handleModo = () => {
-    setModo(!modo);
-    setCita({
-      ...cita,
-      modo: !modo ? "Presencial" : "Virtual",
-    });
-  };
   document.title = oneSchool?.nombre_colegio?.length > 0 ? oneSchool.nombre_colegio : "MiCole"
 
 
 
-  const handleSubmitLista = (e) => {
-    e.preventDefault();
-    if (!isAuth) {
-      Swal.fire({
-        icon: "info",
-        title: "Inicia Sesion",
-        text: "Debes iniciar sesion o registrarte para comentar",
 
-        confirmButtonText: "Iniciar Sesion",
-
-      }).then(res => {
-        if (res.isConfirmed) {
-          setOpenLogin(true);
-        }
-      });
-      return;
-    } else {
-      try {
-        let data = {
-          año: Number(ingresoParams),
-          gradoId: Number(gradoParams),
-          usuarioId: user?.id,
-          colegioId: oneSchool?.id,
-        };
-        axios
-          .post("/lista", data)
-          .then((res) => {
-            Swal.fire({
-              icon: "success",
-              title: "Lista creada exitosamente!",
-              text: "Lista Agendada",
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-            Swal.fire({
-              icon: "error",
-              title: "Algo salio mal",
-              text: err.response.data.message,
-            });
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
 
   useEffect(() => {
     if (isAuth) {
@@ -313,11 +188,6 @@ function SchoolDetail() {
       <div className="bg-[#f6f7f8]  ">
         {images.open && <SliderC setImages={setImages} images={images.src}></SliderC>}
         {oneSchool?.primera_imagen?.length > 0 ?
-          // <img
-          //   src={oneSchool.primera_imagen}
-          //   alt="banner"
-          //   className="object-cover w-full h-[500px]"
-          // /> 
           <SwDetail />
           : <div className="w-full h-[500px] flex justify-center items-center">
             <CircularProgress
@@ -333,13 +203,8 @@ function SchoolDetail() {
             {/* compartir en redes */}
             <div className="flex pt-20  pb-2 flex-row ">
               <span className="flex items-center gap-0">
-                {" "}
-                {/* <FontAwesomeIcon
-                  size="sm"
-                  color="rgb(156 163 175)"
-                  className="bg-white rounded-full p-1"
-                  icon={faShare}
-                /> */}
+             
+             
                 <ShareOutlinedIcon  sx={{color:'#696969', padding:'2px'}}/>
                 <p className="  text-[#696969] pl-1 text-[1.9vh]">
                   Compartir
@@ -347,12 +212,7 @@ function SchoolDetail() {
               </span>
               <span className="flex items-center pl-3 gap-0">
                 {" "}
-                {/* <FontAwesomeIcon
-                  size="sm"
-                  className="bg-white rounded-full p-3"
-                  color="rgb(156 163 175)"
-                  icon={faHeart}
-                /> */}
+             
                 <FavoriteBorderOutlinedIcon sx={{color:'#696969', padding:'2px'}}/>
                 <p className="pl-1  text-[#696969] m-0 text-[1.9vh]">Favoritos
                 </p>
@@ -502,110 +362,17 @@ function SchoolDetail() {
                 <TabPanel value="4">
                   <Ubicacion />
                 </TabPanel>
+                {/* // SACAR UNA CITA    //  Lista de espera */}
                 <TabPanel value="5">
                   {listaParams === "true" ? (
                     //  Lista de espera
                     <FormListaEspera gradoId={Number(gradoParams)} año={Number(ingresoParams)} />
                   ) : (
                     // SACAR UNA CITA
-                    <div className=" bg-white flex flex-col gap-5 rounded-md  w-full">
-                      <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">Solicitar una visita</h2>
-                      <div className={style.divSwipperCitas}>
-                        <SecCitas sendDateHs={handleChangeDateHS} />
-                      </div>
-
-                      {/* FORMULARIO DE LA CITA */}
-                      <form
-                        onSubmit={handleSubmit}
-                        className="w-full flex flex-col gap-7"
-                      >
-                        <div className="flex gap-5">
-                          <input
-                            type="button"
-                            value={"Presencial"}
-                            className={`border w-[120px] ${modo ? "bg-[#0061dd] text-white" : "cursor-pointer"
-                              } py-2 rounded-md shadow-lg duration-300`}
-                            onClick={handleModo}
-                            disabled={modo}
-                          />
-                          <input
-                            type="button"
-                            value={"Virtual"}
-                            className={`border w-[120px] py-2 rounded-md shadow-lg ${!modo ? "bg-[#0061dd] text-white" : "cursor-pointer"
-                              } duration-300`}
-                            onClick={handleModo}
-                            disabled={!modo}
-                          />
-                        </div>
-                        <div className="flex w-full gap-5 justify-between">
-                          {
-                            isAuth ?
-                              <input
-                                name="nombre"
-                                type="text"
-                                value={user?.nombre_responsable + " " + user?.apellidos_responsable}
-                                className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                                placeholder="Nombre"
-                                onChange={(e) => {
-                                  setCita({ ...cita, nombre: e.target.value });
-                                }}
-                                required
-                              />
-                              :
-                              <input
-                                name="nombre"
-                                type="text"
-
-                                className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                                placeholder="Nombre"
-                                onChange={(e) => {
-                                  setCita({ ...cita, nombre: e.target.value });
-                                }}
-                                required
-                              />
-                          }
-                          <input
-                            name="cel"
-                            type="number"
-                            pattern="[0-9]{8,15}"
-                            value={user?.telefono}
-                            required
-                            title="Solo se permiten numeros y entre 8 y 10 caracteres"
-                            className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                            placeholder="Celular"
-                            onChange={(e) => {
-                              setCita({ ...cita, celular: Number(e.target.value) });
-                            }}
-                          />
-                        </div>
-                        <input
-                          name="email"
-                          type="email"
-                          value={user?.email}
-                          className="p-3 border-b-2 border-[#0061dd3a] text-base outline-0 w-full"
-                          placeholder="Correo"
-                          onChange={(e) => {
-                            setCita({ ...cita, correo: e.target.value });
-                          }}
-                          required
-                        />
-                        <button
-                          type="submit"
-                          value="Virtual"
-                          className="border mt-5 mx-auto px-10 py-2 rounded-md shadow-lg bg-[#0061dd] text-white duration-300 cursor-pointer"
-                        >
-                          SOLICITAR VISITA
-                        </button>
-                      </form>
-                      <p className="text-sm p-10">
-                        Al enviar estás aceptando los{" "}
-                        <Link className="text-[#0061dd] hover:underline">
-                          Términos y Condiciones de Uso y la Política de Privacidad
-                        </Link>
-                      </p>
-                    </div>
+                    <CitasD ingresoParams={ingresoParams} nombre_grado={nombre_grado}/>
                   )}
                 </TabPanel>
+                {/* Eventos */}
                 <TabPanel value="6">
 
                   {oneSchool?.Eventos?.length > 0 && (
@@ -636,6 +403,7 @@ function SchoolDetail() {
                 <TabPanel value="7">
                   <Comentarios id={id} />
                 </TabPanel>
+                {/* GALERIA / VIDEO */}
                 <TabPanel value="8">
                   <div className="p-5 bg-white flex flex-col gap-5 rounded-md shadow-md w-full">
                     <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">Galería</h2>
--- client/src/pages/SchoolDetail/Form-citas/FormCitas.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FormCitas() {
-  return (
-    <div>FormCitas</div>
-  )
-}
