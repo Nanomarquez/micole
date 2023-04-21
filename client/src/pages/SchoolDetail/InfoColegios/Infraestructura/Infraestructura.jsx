fd98933b14b5bb8aec9692e5656ef93733b6ca42
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-21 03:33:33 +0100

    Modularizacion School Detail  (#134)
    * Update swiperCitas.module.css
    
    * Modularizado-1
    
    limpieza de codigo viejo y reconocimiento de areas en el codigo
    
    * Update Payment.module.css
    
    * medio punto
    
    * css
    
    * css tabs
    
    * css tabs responsive
    
    * css citas tabs responsive
    
    * bugs citas
    
    * responsive css
    
    * se hizo lo que se pudo
    
    * swipper
    
    * swipper primera imagen
    
    * Update SchoolDetail.jsx
    
    * Update SchoolD.module.css
    
    * Update SchoolDetail.jsx
    
    * Update SchoolDetail.jsx
    
    * Update SchoolDetail.jsx
    
    * Update SchoolDetail.jsx
    
    * Update Comentarios.jsx
    
    * Update SwDetail.jsx
    
    * Update SchoolDetail.jsx

---
+++ client/src/pages/SchoolDetail/InfoColegios/Infraestructura/Infraestructura.jsx
@@ -0,0 +1,204 @@
+import React, { useState } from 'react'
+// import { a11yProps, TabPanel } from '../../../components/Tabs';
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import { a11yProps, TabPanel } from '../../../../components/Tabs';
+import { useSelector } from 'react-redux';
+export default function Infraestructura() {
+    const [value, setValue] = useState(0);
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+
+    };
+    
+    const { oneSchool, grados, horariosColegio } = useSelector(
+        (state) => state.schools
+      );
+      
+    let infra = Array.from(
+        new Set(oneSchool?.Infraestructuras?.map((e) => e.InfraestructuraTipoId))
+    );
+    return (
+        <div className=" bg-white flex flex-col gap-5 rounded-md ">
+            <h2 className="font-semibold  text-[#0D263B] text-[2.4vh]">Infraestructura</h2>
+
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+                variant="scrollable"
+                scrollButtons
+                allowScrollButtonsMobile
+            >
+                {infra.map((e) => (
+                    <Tab
+                        style={{
+                            fontSize: "1.6vh",
+                            fontFamily: "Poppins",
+                            textTransform: "capitalize",
+                        }}
+                        label={
+                            e === 1
+                                ? "Administrativo"
+                                : e === 2
+                                    ? "Artistica"
+                                    : e === 3
+                                        ? "Deportiva"
+                                        : e === 4
+                                            ? "Enseñanza"
+                                            : e === 5
+                                                ? "Laboratorio"
+                                                : null
+                        }
+                        {...a11yProps(e)}
+                    />
+                ))}
+            </Tabs>
+
+            <div className="text-sm flex w-full justify-center">
+                {oneSchool?.Infraestructuras?.some(
+                    (e) => e.InfraestructuraTipoId === 1
+                ) && (
+                        <TabPanel value={value} index={0}>
+                            <ul className="grid grid-cols-3 w-full gap-x-10 gap-y-5">
+                                {oneSchool?.Infraestructuras?.filter(
+                                    (e) => e.InfraestructuraTipoId === 1
+                                ).map((e) => (
+                                    <li className="flex items-center gap-3">
+                                        {e.imagen.length > 0 ? (
+                                            <img
+                                                src={e.imagen}
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        ) : (
+                                            <img
+                                                src="https://es.digi.com/getattachment/products/networking/infrastructure-management/icon-im-usbconnectivity.png"
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        )}
+                                        {e.nombre_infraestructura}
+                                    </li>
+                                ))}
+                            </ul>
+                        </TabPanel>
+                    )}
+                {oneSchool?.Infraestructuras?.some(
+                    (e) => e.InfraestructuraTipoId === 2
+                ) && (
+                        <TabPanel value={value} index={1}>
+                            <ul className="grid grid-cols-3 w-full gap-x-10 gap-y-5">
+                                {oneSchool?.Infraestructuras?.filter(
+                                    (e) => e.InfraestructuraTipoId === 2
+                                ).map((e) => (
+                                    <li className="flex items-center gap-3">
+                                        {e.imagen.length > 0 ? (
+                                            <img
+                                                src={e.imagen}
+                                                alt={e.nombre_infraestructura}
+                                              className="w-5"
+                                            />
+                                        ) : (
+                                            <img
+                                                src="https://es.digi.com/getattachment/products/networking/infrastructure-management/icon-im-usbconnectivity.png"
+                                                alt={e.nombre_infraestructura}
+                                              className="w-5"
+                                            />
+                                        )}
+                                        {e.nombre_infraestructura}
+                                    </li>
+                                ))}
+                            </ul>
+                        </TabPanel>
+                    )}
+                {oneSchool?.Infraestructuras?.some(
+                    (e) => e.InfraestructuraTipoId === 3
+                ) && (
+                        <TabPanel value={value} index={2}>
+                            <ul className="grid grid-cols-3 w-full gap-x-10 gap-y-5">
+                                {oneSchool?.Infraestructuras?.filter(
+                                    (e) => e.InfraestructuraTipoId === 3
+                                ).map((e) => (
+                                    <li className="flex items-center gap-3">
+                                        {e.imagen.length > 0 ? (
+                                            <img
+                                                src={e.imagen}
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        ) : (
+                                            <img
+                                                src="https://es.digi.com/getattachment/products/networking/infrastructure-management/icon-im-usbconnectivity.png"
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        )}
+                                        {e.nombre_infraestructura}
+                                    </li>
+                                ))}
+                            </ul>
+                        </TabPanel>
+                    )}
+                {oneSchool?.Infraestructuras?.some(
+                    (e) => e.InfraestructuraTipoId === 4
+                ) && (
+                        <TabPanel value={value} index={3}>
+                            <ul className="grid grid-cols-3 w-full gap-x-10 gap-y-5">
+                                {oneSchool?.Infraestructuras?.filter(
+                                    (e) => e.InfraestructuraTipoId === 4
+                                ).map((e) => (
+                                    <li className="flex items-center gap-3">
+                                        {e.imagen.length > 0 ? (
+                                            <img
+                                                src={e.imagen}
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        ) : (
+                                            <img
+                                                src="https://es.digi.com/getattachment/products/networking/infrastructure-management/icon-im-usbconnectivity.png"
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        )}
+                                        {e.nombre_infraestructura}
+                                    </li>
+                                ))}
+                            </ul>
+                        </TabPanel>
+                    )}
+                {oneSchool?.Infraestructuras?.some(
+                    (e) => e.InfraestructuraTipoId === 5
+                ) && (
+                        <TabPanel value={value} index={4}>
+                            <ul className="grid grid-cols-3 w-full gap-x-10 gap-y-5">
+                                {oneSchool?.Infraestructuras?.filter(
+                                    (e) => e.InfraestructuraTipoId === 5
+                                ).map((e) => (
+                                    <li className="flex items-center gap-3">
+                                        {e.imagen.length > 0 ? (
+                                            <img
+                                                src={e.imagen}
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        ) : (
+                                            <img
+                                                src="https://es.digi.com/getattachment/products/networking/infrastructure-management/icon-im-usbconnectivity.png"
+                                                alt={e.nombre_infraestructura}
+                                                className="w-5"
+                                            />
+                                        )}
+
+                                        {e.nombre_infraestructura}
+                                    </li>
+                                ))}
+                            </ul>
+                        </TabPanel>
+                    )}
+
+            </div>
+        </div>
+    )
+}
